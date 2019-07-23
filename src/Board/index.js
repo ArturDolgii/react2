8688af530853c4db27ffcf41924b5a198392dae0
Artur Dolgii <artur.dolgii@gmail.com>
2019-07-23 19:10:11 +0300

    task 3 * rewrite Boards component and generate cells instead of hardcoding the list

--- src/Board/index.js
+++ src/Board/index.js
@@ -4,32 +4,33 @@ import "./index.css";
 
 class Board extends React.Component {
   renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
+    return <Square key={i}
+                   value={this.props.squares[i]}
+                   onClick={()=>this.props.onClick(i)}/>;
+  }
+
+  createBoard() {
+    let rows = [];
+    let count = -1
+
+    Array(3).fill(null).forEach((item, i) => {
+      let cells = [];
+
+      Array(3).fill(null).forEach((item, j) => {
+        ++count;
+        cells.push(this.renderSquare(count));
+      });
+
+      rows.push(<div key={i} className="board-row">{cells}</div>);
+    })
+
+    return rows;
   }
 
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createBoard()}
       </div>
     );
   }
